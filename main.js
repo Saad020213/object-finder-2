65d155e0d023d1764f89fa1bf782bea4b6f324a3
Saad020213 <117677102+Saad020213@users.noreply.github.com>
2023-11-08 16:19:40 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,27 @@
-objects = [];
-video = "";
-Status = "";
-
-function setup(){
-    canvas = createCanvas(480,360);
-    canvas.center();
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
     video = createCapture(VIDEO);
-    video.size(480,360);
     video.hide();
 }
-function draw(){
-    image(video,0,0,480,360);
-    if(Status != ""){
-    objectDetector.detect(video,gotResults);
-    for(i=0;i<objects.length;i++){
-        document.getElementById("status").innerHTML = "Status : Objects Detected";
-        fill("red");
-        percent = floor(objects[i].confidence*100);
-        text(objects[i].label+" "+percent+"%", objects[i].x+15,objects[i].y+15);
-        noFill();
-        stroke("red");
-        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
 
-        if(objects[i].label == object_name){
-            video.stop();
-            objectDetector.detect(video,gotResults);
-            document.getElementById("number_of_objects").innerHTML = object_name+" Found";
-            synth = window.speechSynthesis;
-            utterThis = new SpeechSynthesisUtterance(object_name+"Found");
-            synth.speak(utterThis);
-        }
-        else{
-            document.getElementById("number_of_objects").innerHTML = object_name+" not Found";
-        }
-    }
-    }
-}
-function start(){
-    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    object_name = document.getElementById("Object_names").value;
-}
-function modelLoaded(){
-    console.log("Model Loaded!");
-    Status = true;
-    video.loop();
-    video.volume(0);
-    video.speed(1);
+function draw() {
+    image(video, 230, 150, 220, 200);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 426, 80);
+    circle(590, 50, 80);
+    circle(50, 50, 80); 
+    circle(590, 426, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(40, 90, 25, 295);
+    rect(580, 90, 25, 295);
 }
 
-function gotResults(error,results){
-    if(error){
-        console.error(error);
-    }
-    console.log(results);
-    objects = results; 
+function take_snapshot() {
+    save('Nice_Face.png');
 }
